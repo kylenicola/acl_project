6bdc78e9c0071e7fdd5ba95259cf66ebda880d15
Kyle Nicola <kyle.a.nicola@gmail.com>
2014-07-06 11:41:32 -0500

    updating urls.py

--- acl_project/urls.py
+++ acl_project/urls.py
@@ -8,7 +8,8 @@ urlpatterns = patterns('',
     # url(r'^$', 'acl_project.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^$', include('acl_app.urls')),
+    url(r'^acl_app', include('acl_app.urls')),
+    url(r'^cs373', include('cs373.urls')),
     url(r'^admin/', include(admin.site.urls)),
 
 )
